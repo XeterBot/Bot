55d40deec0d6c554f89b50279d178c445a78f07a
XeterBot <robloxclone002@gmail.com>
2025-03-25 19:43:35 +0700

    Update AntiTamper.lua

--- lua/prometheus/steps/AntiTamper.lua
+++ lua/prometheus/steps/AntiTamper.lua
@@ -1,176 +1,45 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- AntiTamper.lua
---
--- This Script provides an Obfuscation Step, that breaks the script, when someone tries to tamper with it.
-
-local Step = require("prometheus.step");
-local Ast = require("prometheus.ast");
-local Scope = require("prometheus.scope");
+local Step = require("prometheus.step")
 local RandomStrings = require("prometheus.randomStrings")
-local Parser = require("prometheus.parser");
-local Enums = require("prometheus.enums");
-local logger = require("logger");
+local Parser = require("prometheus.parser")
+local Enums = require("prometheus.enums")
 
-local AntiTamper = Step:extend();
-AntiTamper.Description = "This Step Breaks your Script when it is modified. This is only effective when using the new VM.";
-AntiTamper.Name = "Anti Tamper";
-
-AntiTamper.SettingsDescriptor = {
-    UseDebug = {
-        type = "boolean",
-        default = true,
-        description = "Use debug library. (Recommended, however scripts will not work without debug library.)"
-    }
-}
-
-function AntiTamper:init(settings)
-	
-end
+local AntiTamper = Step:extend()
+AntiTamper.Description = "Prevents modification of the script."
+AntiTamper.Name = "Anti Tamper"
 
 function AntiTamper:apply(ast, pipeline)
-    if pipeline.PrettyPrint then
-        logger:warn(string.format("\"%s\" cannot be used with PrettyPrint, ignoring \"%s\"", self.Name, self.Name));
-        return ast;
-    end
-	local code = "do local valid = true;";
-    if self.UseDebug then
-        local string = RandomStrings.randomString();
-        code = code .. [[
-            -- Anti Beautify
-            local sethook = debug and debug.sethook or function() end;
-            local allowedLine = nil;
-            local called = 0;
-            sethook(function(s, line)
-                called = called + 1;
-                if allowedLine then
-                    if allowedLine ~= line then
-                        sethook(error, "l");
-                    end
-                else
-                    allowedLine = line;
-                end
-            end, "l");
-            (function() end)();
-            (function() end)();
-            sethook();
-            if called < 2 then
-                valid = false;
-            end
+    if pipeline.PrettyPrint then return ast end
 
-            -- Anti Function Hook
-            local funcs = {pcall, string.char, debug.getinfo}
-            for i = 1, #funcs do
-                if debug.getinfo(funcs[i]).what ~= "C" then
-                    valid = false;
-                end
-            end
+    local originalChecksum = "1234567890" -- Chèn checksum thật khi build
+    local randomString = RandomStrings.randomString()
 
-            -- Anti Beautify
-            local function getTraceback()
-                local str = (function(arg)
-                    return debug.traceback(arg)
-                end)("]] .. string .. [[");
-                return str;
+    local code = [[
+        local function hash(str)
+            local h = 0
+            for i = 1, #str do
+                h = (h * 31 + str:byte(i)) % 2^32
             end
-    
-            local traceback = getTraceback();
-            valid = valid and traceback:sub(1, traceback:find("\n") - 1) == "]] .. string .. [[";
-            local iter = traceback:gmatch(":(%d*):");
-            local v, c = iter(), 1;
-            for i in iter do
-                valid = valid and i == v;
-                c = c + 1;
-            end
-            valid = valid and c >= 2;
-        ]]
-    end
-    code = code .. [[
-    local gmatch = string.gmatch;
-    local err = function() error("Tamper Detected!") end;
-
-    local pcallIntact2 = false;
-    local pcallIntact = pcall(function()
-        pcallIntact2 = true;
-    end) and pcallIntact2;
-
-    local random = math.random;
-    local tblconcat = table.concat;
-    local unpkg = table and table.unpack or unpack;
-    local n = random(3, 65);
-    local acc1 = 0;
-    local acc2 = 0;
-    local pcallRet = {pcall(function() local a = ]] .. tostring(math.random(1, 2^24)) .. [[ - "]] .. RandomStrings.randomString() .. [[" ^ ]] .. tostring(math.random(1, 2^24)) .. [[ return "]] .. RandomStrings.randomString() .. [[" / a; end)};
-    local origMsg = pcallRet[2];
-    local line = tonumber(gmatch(tostring(origMsg), ':(%d*):')());
-    for i = 1, n do
-        local len = math.random(1, 100);
-        local n2 = random(0, 255);
-        local pos = random(1, len);
-        local shouldErr = random(1, 2) == 1;
-        local msg = origMsg:gsub(':(%d*):', ':' .. tostring(random(0, 10000)) .. ':');
-        local arr = {pcall(function()
-            if random(1, 2) == 1 or i == n then
-                local line2 = tonumber(gmatch(tostring(({pcall(function() local a = ]] .. tostring(math.random(1, 2^24)) .. [[ - "]] .. RandomStrings.randomString() .. [[" ^ ]] .. tostring(math.random(1, 2^24)) .. [[ return "]] .. RandomStrings.randomString() .. [[" / a; end)})[2]), ':(%d*):')());
-                valid = valid and line == line2;
-            end
-            if shouldErr then
-                error(msg, 0);
-            end
-            local arr = {};
-            for i = 1, len do
-                arr[i] = random(0, 255);
-            end
-            arr[pos] = n2;
-            return unpkg(arr);
-        end)};
-        if shouldErr then
-            valid = valid and arr[1] == false and arr[2] == msg;
-        else
-            valid = valid and arr[1];
-            acc1 = (acc1 + arr[pos + 1]) % 256;
-            acc2 = (acc2 + n2) % 256;
+            return tostring(h)
         end
-    end
-    valid = valid and acc1 == acc2;
 
-    if valid then else
-        repeat 
-            return (function()
-                while true do
-                    l1, l2 = l2, l1;
-                    err();
-                end
-            end)(); 
-        until true;
-        while true do
-            l2 = random(1, 6);
-            if l2 > 2 then
-                l2 = tostring(l1);
-            else
-                l1 = l2;
+        local scriptSource = [[ ]] .. randomString .. [[ ]]
+        if hash(scriptSource) ~= "]] .. originalChecksum .. [[" then
+            error("Tamper Detected!")
+        end
+
+        -- Anti Inject: Kiểm tra môi trường runtime
+        local env = getfenv()
+        for k, v in pairs(env) do
+            if type(v) == "function" and not k:find("_G") then
+                error("Injection Detected!")
             end
         end
-        return;
-    end
-end
-
-    -- Anti Function Arg Hook
-    local obj = setmetatable({}, {
-        __tostring = err,
-    });
-    obj[math.random(1, 100)] = obj;
-    (function() end)(obj);
-
-    repeat until valid;
     ]]
 
-    local parsed = Parser:new({LuaVersion = Enums.LuaVersion.Lua51}):parse(code);
-    local doStat = parsed.body.statements[1];
-    doStat.body.scope:setParent(ast.body.scope);
-    table.insert(ast.body.statements, 1, doStat);
+    local parsed = Parser:new({LuaVersion = Enums.LuaVersion.Lua51}):parse(code)
+    table.insert(ast.body.statements, 1, parsed.body.statements[1])
 
-    return ast;
+    return ast
 end
 
-return AntiTamper;
+return AntiTamper
